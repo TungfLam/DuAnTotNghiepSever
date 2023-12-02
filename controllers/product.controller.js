32227a7e17b5dd370d3b33dd6535fc7b14363b2b
trinh-nghean <trinhtut37@gmail.com>
2023-12-02 11:47:28 +0700

    upload ảnh  cloudinary
    upload ảnh  cloudinary

--- controllers/product.controller.js
+++ controllers/product.controller.js
@@ -2,6 +2,7 @@ var model = require('../models/product.model');
 var modelCategories = require('../models/category.model')
 var modelColor = require('../models/color.model')
 var modelSize = require('../models/sizes.model')
+var cloudinary = require('cloudinary').v2;
 
 
 var model_product_size_color = require('../models/product_size_color.model')
@@ -12,7 +13,8 @@ const { DateTime } = require('luxon');
 let heading = 'Danh sách sản phẩm'
 let title = 'Sản phẩm'
 let ExcelJS = require('exceljs')
-let sharp = require('sharp')
+let sharp = require('sharp');
+const { url } = require('inspector');
 
 const getlistproduct = async (req, res) => {
     const itemsPerPage = 10;
@@ -20,7 +22,7 @@ const getlistproduct = async (req, res) => {
     const startCount = (page - 1) * itemsPerPage + 1;
     const skip = (page - 1) * itemsPerPage;
     const limit = itemsPerPage;
-    const listProducts = await model.productModel.find().skip(skip).limit(limit)
+    const listProducts = await model.productModel.find().skip(skip).limit(limit).sort({createdAt:-1})
         .populate('category_id', "name");
     const listCategory = await modelCategories.categoryModel.find()
     const countProducts = await model.productModel.count(); // Tính tổng số sản phẩm
@@ -117,29 +119,17 @@ const addDetail = async (req, res) => {
 const addproduct = async (req, res) => {
     let message = '';
     const { name, description, price, category } = req.body;
-    const image = [];
-    const nowInVietnam = Date.now();
-
-    // Xử lý tất cả các tệp hình ảnh đã tải lên
-    for (const file of req.files) {
-        try {
-            const imageBuffer = fs.existsSync(file.path) ? fs.readFileSync(file.path) : null;
-            const pngBuffer = await sharp(imageBuffer).toFormat('png').toBuffer();
-            const base64Image = pngBuffer.toString('base64');
-            image.push(base64Image);
-        } catch (error) {
-            console.error('Lỗi chuyển đổi AVIF sang PNG:', error);
-            res.status(500).json({ message: 'Lỗi chuyển đổi ảnh: ' + error.message });
-            return; 
-        }
-    }
-
+    const image = []
+    const fileData = req.files
+    fileData.forEach(item => {
+        image.push(item.path)
+    });
     if (req.method === 'POST') {
         let objProduct = new model.productModel({
             name: name,
             description: description,
             category_id: category,
-            createdAt: nowInVietnam,
+            createdAt: Date.now(),
             image: image,
             price: price,
             heading: heading,
@@ -154,7 +144,11 @@ const addproduct = async (req, res) => {
         }
     }
 };
-
+function getPublicIdFromUrl(url) {
+    const startIndex = url.lastIndexOf('/') + 1;
+    const endIndex = url.lastIndexOf('.');
+    return url.substring(startIndex, endIndex);
+}
 
 const deleteproduct = async (req, res) => {
     try {
@@ -164,6 +158,18 @@ const deleteproduct = async (req, res) => {
             countPages -= 1
         }
         let id = req.params.id;
+        const finproduct = await model.productModel.findById(id);
+        finproduct.image.map(url => {
+            const publicId = getPublicIdFromUrl(url);
+            cloudinary.uploader.destroy(publicId, (error, result) => {
+                if (error) {
+                    console.error(`Lỗi khi xóa ảnh với public ID ${publicId}:`, error);
+                } else {
+                    console.log(`Ảnh với public ID ${publicId} đã được xóa. Kết quả:`, result);
+                }
+            });
+        });
+
         await model.productModel.findByIdAndDelete(id);
         await model_product_size_color.product_size_color_Model.deleteMany({ product_id: id });
         res.redirect(`/product/listproduct/${countPages}`)
@@ -173,72 +179,40 @@ const deleteproduct = async (req, res) => {
 
     }
 }
-// const updateproduct = async (req, res) => {
-//     let id = req.params.id;
-//     let title = 'Update Product'
 
-//     try {
-//         var itemedit = await model.productModel.findById(id);
-//     } catch (error) {
-//     }
-
-//     const nowInVietnam = DateTime.now().setZone('Asia/Ho_Chi_Minh');
-//     if (req.method == 'POST') {
-//         try {
-//             var image = [];
-//             for (const file of req.files) {
-//                 const imageBuffer = fs.readFileSync(file.path);
-//                 // mã hóa base64
-//                 const base64Image = imageBuffer.toString('base64');
-//                 image.push(base64Image);
-//             }
-
-//         } catch (error) {
-
-//         }
-//         let objProduct = new model.productModel();
-
-//         objProduct.name = req.body.name
-//         objProduct.description = req.body.description
-//         if (image.length === 0) {
-//             console.log("Mảng rỗng");
-//         } else {
-//             objProduct.image = image;
-//             console.log(image);
-//         }
-//         objProduct.price = req.body.price
-//         objProduct.updatedAt = nowInVietnam
-//         objProduct._id = id;
-
-//         try {
-//             await model.productModel.findByIdAndUpdate({ _id: id, objProduct }, objProduct);
-//             res.redirect('/product/listproduct/1');
-//         } catch (error) {
-//             res.status(500).json({ message: 'Lỗi ghi CSDL: ' + error.message });
-//         }
-//     }
-
-//     res.render('product/updateproduct', { title: title, itemedit: itemedit })
-// }
 const updateproduct = async (req, res) => {
     let id = req.params.id;
     let title = 'Update Product'
 
     try {
         var itemedit = await model.productModel.findById(id);
+        var listCategory = await modelCategories.categoryModel.find()
+        
     } catch (error) {
     }
 
-    const nowInVietnam = DateTime.now();
+
     if (req.method == 'POST') {
         try {
+
+            // xóa ảnh cũ khỏi cloud 
+                itemedit.image.map(url=>{
+                    const publicId = getPublicIdFromUrl(url)
+                    cloudinary.uploader.destroy(publicId),(erro,result)=>{
+                        if (erro) {
+                            console.log("Update Product xóa ảnh khỏi cloud không thành công !!");
+                        }else{
+                            console.log("Update Product xóa ảnh khỏi cloud  thành công !!");
+
+                        }
+                    }
+                })
+            //// thêm ảnh vào cloud 
             var image = [];
-            for (const file of req.files) {
-                const imageBuffer = fs.readFileSync(file.path);
-                // mã hóa base64
-                const base64Image = imageBuffer.toString('base64');
-                image.push(base64Image);
-            }
+            const fileData = req.files
+            fileData.forEach(item => {
+                image.push(item.path)
+            });
 
         } catch (error) {
 
@@ -251,8 +225,8 @@ const updateproduct = async (req, res) => {
             console.log(image);
         }
         itemedit.price = req.body.price
-        itemedit.updatedAt = nowInVietnam
-
+        itemedit.updatedAt = DateTime.now()
+        itemedit.category_id = req.body.category
         try {
             await model.productModel.findByIdAndUpdate(id, itemedit);
             res.redirect('/product/listproduct/1');
@@ -265,7 +239,8 @@ const updateproduct = async (req, res) => {
         {
             title: title,
             itemedit: itemedit,
-            heading: heading
+            heading: heading,
+            listCategory:listCategory
         })
 }
 
