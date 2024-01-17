bb479fdadd3f7d03e12c89b5374cfe0017b676c3
DuyNewbie <nbduy4606@gmail.com>
2024-01-18 00:38:29 +0700

    duy
    fix favorite

--- controllers/api/api_favorite.js
+++ controllers/api/api_favorite.js
@@ -1,20 +1,33 @@
 const mdFavorite = require('../../models/favorite.model')
 const mdUser = require('../../models/user.model')
 const mdProduct = require('../../models/product.model')
+
 const addFavorite = async (req, res) => {
     try {
         const idUser = req.params.idUser;
         const idProduct = req.params.idProduct;
         const id_user = await mdUser.userModel.findById(idUser)
         const id_product = await mdProduct.productModel.findById(idProduct);
+
         if (req.method === 'POST') {
-            let objfavorite = new mdFavorite.favorite_Model({
-                user_id: id_user._id,
-                product_id: id_product._id,
-                createdAt:Date.now()
-            })
-            await objfavorite.save();
-            res.status(200).json({ message: "Success" });
+
+            if(id_user && id_product){
+                let objChekFavorite = await mdFavorite.favorite_Model.findOne({user_id : idUser , product_id : idProduct});
+
+                if(!objChekFavorite){
+                    let objfavorite = new mdFavorite.favorite_Model({
+                        user_id: id_user._id,
+                        product_id: id_product._id,
+                        createdAt:Date.now()
+                    })
+                    await objfavorite.save();
+                    res.status(200).json({ message: "Success" });
+                    return
+                }
+            }
+
+            res.status(500).json({ message: "ERROR : user không tồn tại" });
+            
 
         }
     } catch (error) {
